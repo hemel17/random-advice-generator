805677d86be01c7f5d33d0570eb8d923a6a2db4b
Hemel17 <redewanmahmud@gmail.com>
2024-02-07 22:35:20 +0600

    bg image not loading

--- src/components/Advice/Advice/Advice.jsx
+++ src/components/Advice/Advice/Advice.jsx
@@ -4,22 +4,45 @@ import axios from "axios";
 const Advice = () => {
   const url = "https://api.adviceslip.com/advice";
   const [advice, setAdvice] = useState("");
+  const [backgroundImage, setBackgroundImage] = useState("");
+
+  const backgroundImages = [
+    "/bg-image (1).svg",
+    "/bg-image (2).svg",
+    "/bg-image (3).svg",
+    "/bg-image (4).svg",
+    "/bg-image (5).svg",
+    "/bg-image (6).svg",
+    "/bg-image (7).svg",
+    "/bg-image (8).svg",
+    "/bg-image (8).svg",
+    "/bg-image (10).svg",
+  ];
 
   const loadAdvice = async () => {
     try {
+
+      const randomIndex = Math.floor(Math.random() * backgroundImages.length);
+      setBackgroundImage(backgroundImages[randomIndex]);
+      
       const response = await axios.get(url);
       setAdvice(response.data.slip.advice);
+
+      
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    loadAdvice();
+    loadAdvice()
   }, []);
 
   return (
-    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-h-screen flex justify-center items-center w-full">
+    <div
+      className="bg-cover bg-center min-h-screen flex justify-center items-center w-full"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
       <div className="w-4/5 md:w-3/5 text-center text-white text-3xl md:text-5xl bg-[rgba(0,0,0, 0.3)]">
         <h2 className="mb-4 md:mb-6">{advice}</h2>
         <button
